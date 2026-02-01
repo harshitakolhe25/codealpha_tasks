3c200e90d6d87fbce62074d2ed3b04d316b09515
harshitakolhe25 <harshitakolhe25@gmail.com>
2026-02-01 13:07:56 +0530

    Add files via upload

---
+++ task_1.py
@@ -0,0 +1,39 @@
+import random
+
+words=["apple","dog","cat","fish","elephant"]
+secret_word=random.choice(words)
+
+display=["_"]*len(secret_word)
+count=0
+max_attempts=6
+guessed_words=[]
+
+#for progress 
+while count<max_attempts and "_" in display:
+    print("\n Word: "," ".join(display))
+    print("guessed word: ",guessed_words)
+    print("attempts left: ",max_attempts-count)
+
+    guess=input("Enter the letter: ").lower()
+
+    if guess in guessed_words:
+        print("Already used letter.")
+        continue
+
+    guessed_words.append(guess)
+
+    if guess in secret_word:
+        print("correct guess")
+        for i in range(len(secret_word)):
+            if secret_word[i]==guess:
+                display[i]=guess
+
+    else:
+        print("Guess is wrong")
+        count+=1
+
+if "_" not in display:
+    print("\nCongratulations you won,the word was: ",secret_word)
+
+else:
+    print("Fail you guessed wrong")
---
+++ task_2.py
@@ -0,0 +1,21 @@
+stock_prices={
+    "AAPL":180,
+    "TSLA":250,
+    "GOOG":140
+}
+
+total_value=0
+print("Available stocks:",stock_prices)
+
+n=int(input("How many different stocks do you have? "))
+
+for i in range(n):
+    stock_name=input("Enter stock name: ").upper()
+    quantity=int(input("Enter quantity: "))
+
+    if stock_name in stock_prices:
+        total_value=total_value+(stock_prices[stock_name]*quantity)
+    else:
+        print("Stock not found!")
+
+print("Total investement value is: ",total_value)
